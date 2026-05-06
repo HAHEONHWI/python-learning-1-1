ef030dd2c6138c1c226b5c5eb4e6cdfe679d3be2
Ha HeonHwi <2010haheon@gmail.com>
2026-05-06 13:19:37 +0900

    l

---
+++ 0506/hanoitower.py
@@ -0,0 +1,9 @@
+def hanoi(n, start, end, temp):
+    if n == 1:
+        print(f"{start} -> {end}")
+        return
+
+    hanoi(n - 1, start, temp, end)
+    print(f"{start} -> {end}")
+    hanoi(n - 1, temp, end, start)
+
